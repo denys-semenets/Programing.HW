d79614ba69f3cbaed35a9bd59d3da13727705dfe
denys-semenets <dsemenets@kse.org.ua>
2025-06-08 23:55:06 +0300

    Update HW3.py

--- HW3.py
+++ HW3.py
@@ -39,3 +39,42 @@ print(df["data"]%x == 0 )
 '''
 Medium level
 '''
+list2 = [10, 25, "orange", 45, "banana", 100, "kiwi", 5, 0, "melon"]
+print(df[(df["data"]% x == 0) & (df["data"] % y != 0)])
+
+
+flatten_list = [[1, 2], [3, 4, 5], [6], [7, 8, 9]]
+print([a for b in flatten_list for a in b])
+
+
+
+print([a for a in list1 if isinstance(a,int)and a<8]+ [a for a in list2 if isinstance(a,int) and a>4])
+dicts = [
+    {'a': 10, 'b': 5},
+    {'a': 7, 'b': 3, 'c': 8},
+    {'a': 2, 'c': 4}
+]
+sum = 0
+key = "a"
+for a in dicts:
+    if key in a:
+            sum += a[key]
+print(sum)
+
+print(["kse" if isinstance(a,str) and a.lower().startswith(("c","v","o")) else a for a in list1])
+
+cou = 0
+for a in list1:
+        if isinstance(a,str) and len(a) > x:
+                cou+=1
+print(cou)
+
+conc = []
+
+
+
+for i in range(len(list2)):
+        conc.append(str(list2[i])+str(list1[i]))
+print(conc)
+
+print([a*y if isinstance(a,(int,float)) and a>x else a for a in list1])
